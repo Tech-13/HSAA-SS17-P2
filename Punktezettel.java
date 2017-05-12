65043978f0d1a2bca17ff69b97895f2e0be86652
Benjamin <a>
2017-05-12 18:31:50 +0200

    Version 0.0.2 Sechs Würfel würfeln Einzelne Würfel an und abmarkieren Alle Würfel abmarkieren
    +Punkte ermitteln

--- Punktezettel.java
+++ Punktezettel.java
@@ -1,33 +1,90 @@
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.util.Vector;
 
-/**
- * Write a description of class Punktezettel here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Punktezettel
-{
-    // instance variables - replace the example below with your own
-    private int x;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn; 
+public class Punktezettel extends JFrame {
 
-    /**
-     * Constructor for objects of class Punktezettel
-     */
-    public Punktezettel()
-    {
-        // initialise instance variables
-        x = 0;
+    private final int small = 50;
+    private final int big = 150;
+
+    private JTable table;
+
+    public Punktezettel() {
+        table = new JTable(new SimpleTableModel());
+        setColumnWidth();
+        JScrollPane sPane = new JScrollPane(table);
+
+        getContentPane().add(sPane);
+        this.setSize(600, 300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    private void setColumnWidth() {
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            TableColumn c = table.getColumnModel().getColumn(i);
+            switch (i) {
+            case 1:
+                c.setPreferredWidth(JTable.AUTO_RESIZE_ALL_COLUMNS);
+                break;
+            case 2:
+                c.setPreferredWidth(JTable.AUTO_RESIZE_ALL_COLUMNS);
+                break;
+            case 3:
+                c.setPreferredWidth(JTable.AUTO_RESIZE_ALL_COLUMNS);
+                break;
+            case 4:
+                c.setPreferredWidth(JTable.AUTO_RESIZE_ALL_COLUMNS);
+                break;
+            default:
+                c.setPreferredWidth(small);
+            }
+        }
     }
+
+    public static void main(String[] args) {
+        new Punktezettel();
+    }
+
 }
+
+class SimpleTableModel extends DefaultTableModel {
+
+    private int rows = 30, cols = 5;
+    
+    private Object[] rowData = new Object[cols];
+
+    public SimpleTableModel() {
+        super();
+        initModelData();
+    }
+
+    private void initModelData() {
+        
+        for (int i = 0; i < cols; i++) {
+            this.addColumn(Integer.toString(i));
+        }
+
+      
+        for (int j = 0; j < 1; j++) {
+            for (int i = 1; i < cols; i++) {
+                rowData[i] = i +". Spieler";
+            }
+            this.addRow(rowData);
+        }
+        
+        
+        
+       
+        
+        
+        
+
+        
+ 
+
+    }
+} 
--- Würfelbecher.java
+++ Würfelbecher.java
@@ -11,7 +11,7 @@ public class Würfelbecher
     private int Würfel1;    private int Würfel2;    private int Würfel3;    private int Würfel4;    private int Würfel5;    private int Würfel6;
     private boolean Würfel1b;    private boolean Würfel2b;    private boolean  Würfel3b;    private boolean  Würfel4b;    private boolean  Würfel5b;    private boolean  Würfel6b;
     private int Punktzahl;
-
+    int p = 0;
     /**
      * Constructor for objects of class Würfelbecher
      */
@@ -76,35 +76,64 @@ public class Würfelbecher
     { Würfel1b = false; Würfel2b = false; Würfel3b = false; Würfel4b = false; Würfel5b = false; Würfel6b = false; 
       return Würfel1b &&  Würfel2b && Würfel3b && Würfel4b && Würfel5b && Würfel6b;}  
     
-    
-    
+
     
     
     /**   Methode um Punkte einzutragen */    
     public String eintragen(String Feld)
     {
+        
+        StringBuilder builder = new StringBuilder(Feld.substring(0, 1));
+        int a = Integer.parseInt(builder.toString());        
+        
+        if(Würfel1 == a){p=p+a;}
+        if(Würfel2 == a){p=p+a;}
+        if(Würfel3 == a){p=p+a;}
+        if(Würfel4 == a){p=p+a;}
+        if(Würfel5 == a){p=p+a;}
+        if(Würfel6 == a){p=p+a;}        
+        
+        
         //Eintragen der Punktzahl
         switch (Feld) {
-            case "1er":  Punktzahl = 0;
+            case "1er":  Punktzahl = p;
                      break;
-            case "2er":  Punktzahl = 0;
+            case "2er":  Punktzahl = p;
                      break;
-            case "3er":  Punktzahl = 0;
+            case "3er":  Punktzahl = p;
                      break;
-            case "4er":  Punktzahl = 0;
+            case "4er":  Punktzahl = p;
                      break;
-            case "5er":  Punktzahl = 0;
+            case "5er":  Punktzahl = p;
                      break;
-            case "6er":  Punktzahl = 0;
-                     break;                     
+            case "6er":  Punktzahl = p;
+                     break;
+            case "Pasch":  Punktzahl = Würfel1 + Würfel2 + Würfel3 + Würfel4 + Würfel5 + Würfel6;
+                     break;
+            case "3er-Pasch":  Punktzahl = Würfel1 + Würfel2 + Würfel3 + Würfel4 + Würfel5 + Würfel6;
+                     break; 
+            case "4er-Pasch":  Punktzahl = Würfel1 + Würfel2 + Würfel3 + Würfel4 + Würfel5 + Würfel6;
+                     break;
+            case "Kleine Straße":  Punktzahl = 35;
+                     break;
+            case "Große Straße":  Punktzahl = 45;
+                     break;
+            case "Full House":  Punktzahl = 40;
+                     break;
+            case "Kniffel":  Punktzahl = 75;
+                     break;          
             default: Punktzahl = -1;
                      break;
         }
+        
+       p=0;
+        
+        System.out.println(Punktzahl);
         return Feld;
     }    
         
        
-        
+
         
         
         
