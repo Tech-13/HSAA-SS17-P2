924bfcb111c1b8dc6b407792614dc20b13ebe09d
LuisB <lui5b@users.noreply.github.com>
2017-05-22 20:57:03 +0200

    Doku zur Spieler-Klasse hinzugefügt

--- Spieler.java
+++ Spieler.java
@@ -1,36 +1,42 @@
-
+/**
+ * Mit der Klasse Spieler kann für jeden am Spiel teilnehmenden Spieler ein Objekt erzeugt werden, 
+ * in dem Daten wie Name und Aktivität gespeichert werden, und das die entsprechenden Methoden bereitstellt.
+ */
 
 public class Spieler
 {
-    private String name;
-    private boolean aktivität;
+    private String name; //Name des Spielers
+    private boolean aktivität; //Ist der jeweilige Spieler gerade an der Reihe oder nicht?
     
-    public Spieler()
+    public Spieler() //Aktivität des Spielers wird mit "nicht aktiv" initiiert
     {
         aktivität = false;
     }
     
-    public void spielernameEingeben(String spielername)
+    public void spielernameEingeben(String spielername) //Methode zum Eingeben des Spielernamens
     {
         name = spielername;
     }
     
-    public String spielernamenAusgeben()
-    {
+    /** Methode zum Ausgeben des Spielernamens
+     * 
+     * public String spielernamenAusgeben()
+       {
         return name;
-    }
-    
-    public void spielerAktiv()
+       }
+    */
+   
+    public void spielerAktiv() //Methode um die Aktivität des Spielers auf "an der Reihe" zu setzen
     {
         aktivität = true;
     }
     
-    public void spielerNichtAktiv()
+    public void spielerNichtAktiv() //Methode um die Aktivität des Spielers auf "nicht an der Reihe" zu setzen
     {
         aktivität = false;
     }
     
-    public boolean spielerAktivät()
+    public boolean spielerAktivät() //Methode zum Feststellen ob Spieler aktiv ist oder nicht
     {
         return aktivität;
     }
