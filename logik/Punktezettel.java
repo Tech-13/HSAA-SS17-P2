252232b42da96e9e4ce23f1681f207ae294ffa35
Jake <ja.ke@posteo.de>
2017-05-26 00:35:14 +0200

    Straßenberechnung implementiert und Pasch Methoden angepasst

--- logik/Punktezettel.java
+++ logik/Punktezettel.java
@@ -43,13 +43,13 @@ public class Punktezettel {
         } else {
         	switch (kombiTyp) {
 				case Kombi.PASCH_3X:
-					punkte = check_Pasch(würfel, 3)? augenzahlSumme(würfel) : 0; 
+					punkte = check_Pasch3(würfel)? augenzahlSumme(würfel) : 0; 
 					break;
 				case Kombi.PASCH_4X:
-					punkte = check_Pasch(würfel, 4)? augenzahlSumme(würfel) : 0; 
+					punkte = check_Pasch4(würfel)? augenzahlSumme(würfel) : 0; 
 					break;
 				case Kombi.PASCH_5X:
-					punkte = check_Pasch(würfel, 5)? augenzahlSumme(würfel) : 0; 
+					punkte = check_Pasch5(würfel)? augenzahlSumme(würfel) : 0; 
 					break;				
 				case Kombi.FULL_HOUSE:
 					punkte = check_FullHouse(würfel)? 25 : 0;
@@ -95,26 +95,36 @@ public class Punktezettel {
 	}
     
     private boolean check_KleineStraße(byte[] sortierteWürfel) {
-		
+    	byte[] eins = {1,2,3,4};
+    	byte[] zwei = {2,3,4,5};
+    	byte[] drei = {3,4,5,6};
+    	byte[] anfang = Arrays.copyOfRange(sortierteWürfel, 0, 4);
+    	byte[] ende = Arrays.copyOfRange(sortierteWürfel, 1, 5);
+
+    	boolean str1 = Arrays.equals(anfang, eins) || Arrays.equals(anfang, zwei) || Arrays.equals(anfang, drei);
+    	boolean str2 = Arrays.equals(ende, eins) || Arrays.equals(ende, zwei) || Arrays.equals(ende, drei);
+    	return str1 || str2;
 	}
     private boolean check_GroßeStraße(byte[] sortierteWürfel) {
-
+    	byte[] eins = {1,2,3,4,5};
+    	byte[] zwei = {2,3,4,5,6};
+    	return Arrays.equals(sortierteWürfel, eins) || Arrays.equals(sortierteWürfel, zwei);
 	}
     
-    private boolean check_Pasch(byte[] sortierteWürfel, 3) {
+    private boolean check_Pasch3(byte[] sortierteWürfel) {
 	boolean P31 = (sortierteWürfel[4]) == (sortierteWürfel[3]) && (sortierteWürfel[3]) == (sortierteWürfel[2]);
 	boolean P32 = (sortierteWürfel[3]) == (sortierteWürfel[2]) && (sortierteWürfel[2]) == (sortierteWürfel[1]);
 	boolean P33 = (sortierteWürfel[2]) == (sortierteWürfel[1]) && (sortierteWürfel[1]) == (sortierteWürfel[0]);
     	return P31 || P32 || P33;
     	}
 
-    private boolean check_Pasch(byte[] sortierteWürfel, 4) {
+    private boolean check_Pasch4(byte[] sortierteWürfel) {
 	boolean P41 = (sortierteWürfel[4]) == (sortierteWürfel[3]) && (sortierteWürfel[3]) == (sortierteWürfel[2]) && (sortierteWürfel[2]) == (sortierteWürfel[1]);
 	boolean P42 = (sortierteWürfel[0]) == (sortierteWürfel[1]) && (sortierteWürfel[1]) == (sortierteWürfel[2]) && (sortierteWürfel[2]) == (sortierteWürfel[3]); 
 	return P41 || P42;
     }
 	    
-    private boolean check_Pasch(byte[] sortierteWürfel, 5) {	    
+    private boolean check_Pasch5(byte[] sortierteWürfel) {	    
 	boolean P50 = (sortierteWürfel[4]) == (sortierteWürfel[3]) && (sortierteWürfel[3]) == (sortierteWürfel[2]) && (sortierteWürfel[2]) == (sortierteWürfel[1]) && (sortierteWürfel[1]) == (sortierteWürfel[0]);
 	return P50;
     	}
