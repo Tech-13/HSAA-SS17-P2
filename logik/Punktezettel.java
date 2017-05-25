28505c4e4021b9cee000444ee8a8cde565fc005e
Jake <ja.ke@posteo.de>
2017-05-25 18:50:50 +0200

    Überprüfung der Kombinationen mit sortierten Würfel Arrays

--- logik/Punktezettel.java
+++ logik/Punktezettel.java
@@ -33,27 +33,35 @@ public class Punktezettel {
      * @param würfel byte[] 
      * @return
      */
-    public int punkteBerechen(int kombiTyp, byte[] würfel, int augen) {
+    public int punkteBerechen(int kombiTyp, byte[] würfel) {
         int punkte = 0;
+        Arrays.sort(würfel);
         
         if(kombiTyp <= 6) {
-        	augen = kombiTyp;
+        	int augen = kombiTyp;
         	punkte = zähleWürfel(würfel, augen)*kombiTyp;
         } else {
         	switch (kombiTyp) {
 				case Kombi.PASCH_3X:
-					
+					punkte = check_Pasch(würfel, 3)? augenzahlSumme(würfel) : 0; 
 					break;
-				// TODO Pasch und Chance
+				case Kombi.PASCH_4X:
+					punkte = check_Pasch(würfel, 4)? augenzahlSumme(würfel) : 0; 
+					break;
+				case Kombi.PASCH_5X:
+					punkte = check_Pasch(würfel, 5)? augenzahlSumme(würfel) : 0; 
+					break;				
 				case Kombi.FULL_HOUSE:
-					punkte = 25;
+					punkte = check_FullHouse(würfel)? 25 : 0;
 					break;
 				case Kombi.KLEINE_STRASSE:
-					punkte = 30;
+					punkte = check_KleineStraße(würfel)? 30 : 0;
 					break;
 				case Kombi.GROSSE_STRASSE:
-					punkte = 40;
-					break;
+					punkte = check_GroßeStraße(würfel)? 40 : 0;
+					break;					
+				case Kombi.CHANCE:
+					punkte = augenzahlSumme(würfel);
 				default:
 					break;
 			}
@@ -71,6 +79,32 @@ public class Punktezettel {
 		}
     	return n;
     }
+    
+    private int augenzahlSumme(byte[] würfel) {
+		int sum = 0;
+		for (byte w : würfel) {
+			sum += w;
+		}
+		return sum;
+	}
+    
+    private boolean check_FullHouse(byte[] sortierteWürfel) {
+    	boolean zwei =  sortierteWürfel[0] == sortierteWürfel[1];
+    	boolean drei = sortierteWürfel[2] == (sortierteWürfel[3]) && (sortierteWürfel[2]) == (sortierteWürfel[4]);
+		return zwei && drei;
+		// TODO 3 gleiche Würfel am Anfang und 2 am Ende
+	}
+    
+    private boolean check_KleineStraße(byte[] sortierteWürfel) {
+		
+	}
+    private boolean check_GroßeStraße(byte[] sortierteWürfel) {
+		
+	}
+    
+    private boolean check_Pasch(byte[] sortierteWürfel, int länge) {
+		
+	}
 
     /**
      * @param kombiTyp 
