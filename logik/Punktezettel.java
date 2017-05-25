e3949899c7722548e37381e408d0750af8eea414
Tech-13 <veretacore@gmail.com>
2017-05-26 00:12:25 +0200

    Pasch Filter ohne Schleife

--- logik/Punktezettel.java
+++ logik/Punktezettel.java
@@ -98,12 +98,26 @@ public class Punktezettel {
 		
 	}
     private boolean check_GroßeStraße(byte[] sortierteWürfel) {
-		
+
 	}
     
-    private boolean check_Pasch(byte[] sortierteWürfel, int länge) {
-		
-	}
+    private boolean check_Pasch(byte[] sortierteWürfel, 3) {
+	boolean P31 = (sortierteWürfel[4]) == (sortierteWürfel[3]) && (sortierteWürfel[3]) == (sortierteWürfel[2]);
+	boolean P32 = (sortierteWürfel[3]) == (sortierteWürfel[2]) && (sortierteWürfel[2]) == (sortierteWürfel[1]);
+	boolean P33 = (sortierteWürfel[2]) == (sortierteWürfel[1]) && (sortierteWürfel[1]) == (sortierteWürfel[0]);
+    	return P31 || P32 || P33;
+    	}
+
+    private boolean check_Pasch(byte[] sortierteWürfel, 4) {
+	boolean P41 = (sortierteWürfel[4]) == (sortierteWürfel[3]) && (sortierteWürfel[3]) == (sortierteWürfel[2]) && (sortierteWürfel[2]) == (sortierteWürfel[1]);
+	boolean P42 = (sortierteWürfel[0]) == (sortierteWürfel[1]) && (sortierteWürfel[1]) == (sortierteWürfel[2]) && (sortierteWürfel[2]) == (sortierteWürfel[3]); 
+	return P41 || P42;
+    }
+	    
+    private boolean check_Pasch(byte[] sortierteWürfel, 5) {	    
+	boolean P50 = (sortierteWürfel[4]) == (sortierteWürfel[3]) && (sortierteWürfel[3]) == (sortierteWürfel[2]) && (sortierteWürfel[2]) == (sortierteWürfel[1]) && (sortierteWürfel[1]) == (sortierteWürfel[0]);
+	return P50;
+    	}
 
     /**
      * @param kombiTyp 
