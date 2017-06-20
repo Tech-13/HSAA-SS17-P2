ab5943934fcf6e755bb901d60dfcae13825caec9
Benjamin <a>
2017-06-20 14:39:33 +0200

    Würfel sind anklickbar

--- gui/Spielfeld.java
+++ gui/Spielfeld.java
@@ -6,16 +6,8 @@ import javax.swing.*; // classes with "J"
 public class Spielfeld extends JFrame {
 
   // declare controls referenced by more than one "block"
-
-
   JButton wurfeln;
-  JButton markieren;
-
-
-
-  // build the ui in some logical order,
-  // using separate methods for each area to keep an overview
-  // the order of method implementation follows invocation order
+  JButton wurf1;
   public Spielfeld() {
     super("Spielfeld");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -68,21 +60,6 @@ public class Spielfeld extends JFrame {
   public JPanel buildWestPanel() {
     JPanel west = new JPanel();
     west.setBackground(Color.GREEN);
-    
-    wurfeln = new JButton("Wurfeln");
-    wurfeln.setEnabled(true);
-    wurfeln.addActionListener((ActionEvent e) -> {
-      System.out.println("gewurfelt");
-    });
-    west.add(wurfeln);
-    
-    markieren = new JButton("Markieren");
-    markieren.setEnabled(true);
-    markieren.addActionListener((ActionEvent e) -> {
-      System.out.println("markieren");
-    });
-    west.add(markieren);
-    
     return west;
   }
 
@@ -97,12 +74,7 @@ public class Spielfeld extends JFrame {
     });
     north.add(wurfeln);
     
-    markieren = new JButton("Markieren");
-    markieren.setEnabled(true);
-    markieren.addActionListener((ActionEvent e) -> {
-      System.out.println("markieren");
-    });
-    north.add(markieren);
+ 
     
     return north;
   }
@@ -113,28 +85,74 @@ public class Spielfeld extends JFrame {
     
     ImageIcon image = new ImageIcon("C:\\Users\\balut\\Documents\\NetBeansProjects\\wurfeltest\\pictures\\1.png");
     JLabel imagelabel = new JLabel(image);
-    center.add(imagelabel);
+    imagelabel.addMouseListener(new MouseAdapter(){
+    public void mouseClicked(MouseEvent e)  {
+     System.out.println("Würfel 1 markiert");}});
+    
     
     ImageIcon image2 = new ImageIcon("C:\\Users\\balut\\Documents\\NetBeansProjects\\wurfeltest\\pictures\\2.png");
     JLabel imagelabel2 = new JLabel(image2);
-    center.add(imagelabel2);
+    imagelabel2.addMouseListener(new MouseAdapter(){
+    public void mouseClicked(MouseEvent e)  {
+     System.out.println("Würfel 2 markiert");}});
     
     ImageIcon image3 = new ImageIcon("C:\\Users\\balut\\Documents\\NetBeansProjects\\wurfeltest\\pictures\\3.png");
     JLabel imagelabel3 = new JLabel(image3);
-    center.add(imagelabel3);
+    imagelabel3.addMouseListener(new MouseAdapter(){
+    public void mouseClicked(MouseEvent e)  {
+     System.out.println("Würfel 3 markiert");}});
+    
     
     ImageIcon image4 = new ImageIcon("C:\\Users\\balut\\Documents\\NetBeansProjects\\wurfeltest\\pictures\\4.png");
     JLabel imagelabel4 = new JLabel(image4);
-    center.add(imagelabel4);
+    imagelabel4.addMouseListener(new MouseAdapter(){
+    public void mouseClicked(MouseEvent e)  {
+     System.out.println("Würfel 4 markiert");}});
+    
     
     ImageIcon image5 = new ImageIcon("C:\\Users\\balut\\Documents\\NetBeansProjects\\wurfeltest\\pictures\\5.png");
     JLabel imagelabel5 = new JLabel(image5);
+    imagelabel5.addMouseListener(new MouseAdapter(){
+    public void mouseClicked(MouseEvent e)  {
+     System.out.println("Würfel 5 markiert");}});
+    
+    
+    ImageIcon image6 = new ImageIcon("C:\\Users\\balut\\Documents\\NetBeansProjects\\wurfeltest\\pictures\\6.png");
+    JLabel imagelabel6 = new JLabel(image6);
+    imagelabel6.addMouseListener(new MouseAdapter(){
+    public void mouseClicked(MouseEvent e)  {
+     System.out.println("Würfel 6 markiert");}});
+    
+    
+    
+   
+    
+    center.add(imagelabel);
+    center.add(imagelabel2);
+    center.add(imagelabel3);
+    center.add(imagelabel4);
     center.add(imagelabel5);
     
     return center;
   }
 
   public static void main(String[] args) {
-    new Spielfeld().setVisible(true);
+    new FirstWindow().setVisible(true);
+    
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   Object rowData[][] = { { " ", " ", " " },
+        { " ", " ", " " } };
+    Object columnNames[] = { "1.Spieler", "2.Spieler", "3Spieler" };
+    JTable table = new JTable(rowData, columnNames);
+    JScrollPane scrollPane = new JScrollPane(table);
+    frame.add(scrollPane, BorderLayout.CENTER);
+    frame.setSize(300, 150);
+    frame.setVisible(true);
+  
+     
+  
   }
 }
+
+
