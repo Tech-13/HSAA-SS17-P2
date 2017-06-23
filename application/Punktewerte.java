5020b36ad0f4ef978c02b20f6b73423ef114b653
Jake <ja.ke@posteo.de>
2017-06-23 12:12:43 +0200

    Punktewerte Klasse für GUI Tabelle

--- application/Punktewerte.java
+++ application/Punktewerte.java
@@ -1,12 +1,43 @@
 package application;
 
-import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleSetProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Punktewerte {
-	private StringProperty spieler1;
-	public Punktewerte() {
-		spieler1 = new SimpleStringProperty(this, "Hallo");
+	public String kombi;
+	private int[] punkte = {0, 0, 0};
+	private int spielerAnzahl = 3;
+	
+	public Punktewerte(String pKombi) {
+		this.kombi = (pKombi);
 	}
+
+	public String getKombi() {
+		return kombi;
+	}
+
+	public void setKombi(String kombi) {
+		this.kombi = kombi;
+	}
+
+	public int[] getPunkte() {
+		return punkte;
+	}
+	
+	public int getPunkt(int nr) {
+		return punkte[nr];
+	}
+
+	public void setPunkte(int[] punkte) {
+		this.punkte = punkte;
+	}
+	
+	public void setPunkt(int nr, int wert) {
+		punkte[nr] = wert;
+	}
+
+	
 }
+	
+	
