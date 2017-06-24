f3a4f6871482461bd79e3818a310765a0cc70f46
Jake <ja.ke@posteo.de>
2017-06-24 20:45:39 +0200

    Startbildschirm hinzufügen

--- application/Main.java
+++ application/Main.java
@@ -1,11 +1,16 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -13,12 +18,14 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	
 	@FXML private TableView tb;
+	private Stage primaryStage;
 	
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
 		try {
-			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,1000,800);
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Start.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			
@@ -26,6 +33,24 @@ public class Main extends Application {
 			
 		} catch(Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+	protected void handleStart(ActionEvent e){
+		try {
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Button btn = (Button)e.getSource();
+			Stage oldStage = (Stage) btn.getScene().getWindow();
+			oldStage.close();
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 	}
 	
