b14946d7bc72dbfec1b00bdaf749a876556a675b
Benjamin <a>
2017-06-29 18:13:29 +0200

    Kombi neu kommentiert

--- logik/Kombi.java
+++ logik/Kombi.java
@@ -8,72 +8,72 @@ import java.util.*;
 public class Kombi {
 		
     /**
-     * Beschreibt wieviele Einser vorhaden sind und zum Punktefeld "1er" zählen
+     *Zur Kombination "1er" zählen nur die gewürfelten Einser, ihre Summe ergibt die Punktzahl.
      */
     public static final int ALLE_1ER = 1;
 
     /**
-     * Beschreibt wieviele Zweier vorhanden sind und zum Punktefeld "2er" zählen
+     *Zur Kombination "2er" zählen nur die gewürfelten Zweier, ihre Summe ergibt die Punktzahl.
      */
     public static final int ALLE_2ER = 2;
 
     /**
-     * Beschreibt wieviele Dreier vorhanden sind und zum Punktefeld "3er" zählen
+     *Zur Kombination "3er" zählen nur die gewürfelten Dreier, ihre Summe ergibt die Punktzahl.
      */
     public static final int ALLE_3ER = 3;
 
     /**
-     * Beschreibt wieviele Vierer vorhanden sind und zum Punktefeld "4er" zählen
+     *Zur Kombination "4er" zählen nur die gewürfelten Vierer, ihre Summe ergibt die Punktzahl.
      */
     public static final int ALLE_4ER = 4;
 
     /**
-     * Beschreibt wieviele Fünfer vorhanden sind und zum Punktefeld "5er" zählen
+     *Zur Kombination "5er" zählen nur die gewürfelten Fünfer, ihre Summe ergibt die Punktzahl.
      */
     public static final int ALLE_5ER = 5;
 
     /**
-     * Beschreibt wieviele Sechser vorhanden sind und zum Punktefeld "6er" zählen
+     *Zur Kombination "6er" zählen nur die gewürfelten Sechser, ihre Summe ergibt die Punktzahl.
      */
     public static final int ALLE_6ER = 6;
 
     /**
-     * Beschreibt ob von einer Würfelzahl mindestens drei vorhanden sind
+     *Zur Kombination 3er Pasch werden drei gleiche augenzahlen benötigt, die Punktzahl ergibt sich aus der Summe aller Augenzahlen.
      */
     public static final int PASCH_3X = 7;
 
     /**
-     * Beschreibt ob von einer Würfelzahl mindestens vier vorhanden sind
+     *Zur Kombination 4er Pasch werden vier gleiche augenzahlen benötigt, die Punktzahl ergibt sich aus der Summe aller Augenzahlen.
      */
     public static final int PASCH_4X = 8;
 
     /**
-     * Beschreibt ob von einer Würfelzahl fünf vorhanden sind
+     *Der Kniffel beschreibt einen Wurf fünf gleicher Augenzahlen und bringt 50 Punkte.
      */
     public static final int PASCH_5X = 9;
 
     /**
-     * Beschreibt ob ein Full House vorliegt, also zwei gleiche Augenzahlen plus drei gleiche andere Augenzahlen
+     *Das Full House beschreibt den Wurf eines Dreierpasches plus eines anderen Pasches und ergibt 25 Punkte.
      */
     public static final int FULL_HOUSE = 10;
 
     /**
-     * Beschreibt die kleine Strasse, also ein aufeinander Reihung vier fortlaufender Augenzaheln
+     *Die kleine Strasse beschreibt eine aufeinander Reihung vier fortlaufender Augenzaheln un bringt 30 Punkte.
      */
     public static final int KLEINE_STRASSE = 11;
 
     /**
-     * Beschreibt die große Strasse, also ein aufeinander Reihung fünf fortlaufender Augenzaheln
+     *Die große Strasse beschreibt eine aufeinander Reihung fünf fortlaufender Augenzaheln un bringt 40 Punkte.
      */
     public static final int GROSSE_STRASSE = 12;
 
     /**
-     * Beschreibt das Feld Chance, bei dem alle Augenzahlen zusammen addiert werden und ein Punktzahl ergeben
+     *Bei der Chance kann jeder Wurf eingetragen werden. Die Punktzahl ergibt sich aus der Summe aller Augenzahlen.
      */
     public static final int CHANCE = 13;
     
     /**
-     * String mit allen Möglichen Punktefeldern
+     *Auflistung aller möglichen Punktefeldern.
      */
     
     public static final String[] bezeichnungen = {
